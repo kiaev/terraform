23f56470da47323e4b3cb077d3a94af8e063b8f0
Kiaev Ivan <dapa_maru@mail.ru>
2022-02-08 22:06:10 +0600

    homework1

--- task2/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-sec_key = "30+soKsdFbrp8XpOlZPfUvF3gUPsXfFaBDSErkWh"
-acc_key = "AKIA3IC4RQXYMONWVXC6"
--- task2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "acc_key" {
-    #default = acc_key
-}
-
-variable "sec_key" {
-    #default = sec_key
-}
-
